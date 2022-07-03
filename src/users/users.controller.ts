f27ab106d1a34248c055092e4077b8b89c942c38
Albert Hernandez Pellicer <alberto.pellicer@coverwallet.com>
2022-07-03 08:35:30 +0200

    feat: create find one in controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
   @Get()
   findAll() {
     return 'Hello world';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns #${id} user`;
   }
 }
