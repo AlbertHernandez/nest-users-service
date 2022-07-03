90e9200a71814cffd54efc9a51581574ef94ab77
Albert Hernandez Pellicer <alberto.pellicer@coverwallet.com>
2022-07-03 09:05:26 +0200

    feat: use users service in the controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,40 +3,40 @@ import {
   Controller,
   Delete,
   Get,
-  HttpCode,
-  HttpStatus,
   Param,
   Patch,
   Post,
   Query,
 } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
   @Get()
   findAll(@Query() paginationQuery) {
-    const { limit, offset } = paginationQuery;
-    return `This action returns all users. Limit ${limit}, offset: ${offset}`;
+    // const { limit, offset } = paginationQuery;
+    return this.usersService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return `This action returns #${id} user`;
+    return this.usersService.findOne(id);
   }
 
   @Post()
-  @HttpCode(HttpStatus.CREATED)
   create(@Body() body) {
-    return body;
+    return this.usersService.create(body);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() body) {
-    return `This action updates #${id} user`;
+    return this.usersService.update(id, body);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return `This action removes #${id} user`;
+    return this.usersService.remove(id);
   }
 }
