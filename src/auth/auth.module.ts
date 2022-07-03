8458103478b377629c20e4e1e609aeb31f394b01
Albert Hernandez Pellicer <alberto.pellicer@coverwallet.com>
2022-07-03 11:01:01 +0200

    feat: get api key from config service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport';
-import { ApiKeyStrategy } from './apiKey.strategy';
+import { ApiKeyStrategy } from './api-key.strategy';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PassportModule],
+  imports: [PassportModule, ConfigModule],
   providers: [AuthService, ApiKeyStrategy],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ConfigIdentifier } from '../config/config-identifier.constant';
+import { ServerConfig } from '../config/config.loader';
 
 @Injectable()
 export class AuthService {
-  private apiKeys: string[] = ['12345'];
+  private apiKeys: string[];
+
+  constructor(private readonly configService: ConfigService) {
+    const serverApiKey = configService.get<ServerConfig>(
+      ConfigIdentifier.Server,
+    );
+    this.apiKeys = [serverApiKey.apiKey];
+  }
 
   validateApiKey(apiKeyToValidate: string) {
     return this.apiKeys.find((apiKey) => apiKey === apiKeyToValidate);
