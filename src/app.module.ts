567867c3aab6b036c6807ea2c86ecc0c6bc05b33
Albert Hernandez Pellicer <alberto.pellicer@coverwallet.com>
2022-07-03 11:05:51 +0200

    feat: move auth middleware to auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
-import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { AuthMiddleware } from './auth/auth.middleware';
 import { ConfigModule } from './config/config.module';
 
 @Module({
@@ -11,8 +10,4 @@ import { ConfigModule } from './config/config.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(AuthMiddleware).forRoutes('');
-  }
-}
+export class AppModule {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport';
 import { ApiKeyStrategy } from './api-key.strategy';
 import { ConfigModule } from '@nestjs/config';
+import { AuthMiddleware } from './auth.middleware';
 
 @Module({
   imports: [PassportModule, ConfigModule],
   providers: [AuthService, ApiKeyStrategy],
 })
-export class AuthModule {}
+export class AuthModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes('');
+  }
+}
