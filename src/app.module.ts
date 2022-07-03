f561f5f8787cc69a3e98993e14d963d6727c20d7
Albert Hernandez Pellicer <alberto.pellicer@coverwallet.com>
2022-07-03 11:06:38 +0200

    feat: remove unused code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from './config/config.module';
 
 @Module({
   imports: [UsersModule, AuthModule, ConfigModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
